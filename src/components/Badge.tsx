fac962c0cb91e74bea3f6244d509d16e426cf372
vekpoke@gmail.com <vekpoke@gmail.com>
2023-09-25 22:10:27 -0400

    Add Website

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -57,7 +57,7 @@ export const Badge = (props: Props) => {
       Powered by
       <a
         ref={liteBadge}
-        href={"https://leonvanzyl.com"}
+        href={"https://trendingneurons.ai"}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
